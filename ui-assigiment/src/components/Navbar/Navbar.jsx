16e7db0871ec243582fb446bb4f5812440a38473
jayprakashlohar <103755935+jayprakashlohar@users.noreply.github.com>
2023-05-29 16:43:43 +0530

    navbar done

--- ui-assigiment/src/components/Navbar/Navbar.jsx
+++ ui-assigiment/src/components/Navbar/Navbar.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React from "react";
+import logo from "./siteLogo.png";
 
 const Navbar = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <>
+      <nav>
+        <div className="logoImage">
+          <img src={logo} alt="" />
+        </div>
+        <div className="navElement">
+          <p>Home</p>
+          <p style={{ color: "#5E57E2" }}>Feature</p>
+          <p>Industry</p>
+          <p>About</p>
+          <p>Contact Us</p>
+        </div>
+        <div className="btnDiv">
+          <button className="demoBtn">Get Demo</button>
+        </div>
+      </nav>
+    </>
+  );
+};
 
-export default Navbar
+export default Navbar;
---
+++ ui-assigiment/src/pages/HomePage.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Navbar from "../components/Navbar/Navbar";
+
+const HomePage = () => {
+  return (
+    <>
+      <div>
+        {/* Navbar */}
+        <Navbar />
+        {/* Navbar */}
+      </div>
+    </>
+  );
+};
+
+export { HomePage };
